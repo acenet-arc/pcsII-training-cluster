00c830ccf9562eb849f24aa2a0e4bb46050c6650
Chris Geroux <chris.m.geroux@gmail.com>
2024-06-10 13:04:49 -0300

    Updating Cloud Images

--- main.tf
+++ main.tf
@@ -14,13 +14,15 @@ module "openstack" {
 
   cluster_name = "pcsii"
   domain       = "ace-net.training"
-  image        = "Rocky-9.3-x64-2023-11"
+  #image        = "Rocky-9.3-x64-2023-11"
+  image        = "Rocky-9.1-x64-2023-02"
 
   instances = {
     mgmt    = { type = "p8-12gb", tags = ["puppet", "mgmt", "nfs"], disk_size=50, count = 1 }
     login   = { type = "p8-12gb", tags = ["login", "public", "proxy"], disk_size=50, count = 1 }
     node16c = { type = "c16-60gb-392", tags = ["node"], count = 1 }
-    node4c1g = { image="Rocky-8.9-x64-2023-11",type = "g1-8gb-c4-22gb", tags = ["node"], count = 1 }
+    #node4c1g = { image="Rocky-8.9-x64-2023-11",type = "g1-8gb-c4-22gb", tags = ["node"], count = 1 }
+    node4c1g = { image="Rocky-8.10-x64-2024-06",type = "g1-8gb-c4-22gb", tags = ["node"], count = 1 }
   }
 
   # var.pool is managed by Slurm through Terraform REST API.
