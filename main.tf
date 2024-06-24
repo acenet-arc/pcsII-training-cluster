3ec84fa7431c6185cd71a07881ad3fe2f0c84ff8
Chris Geroux <chris.m.geroux@gmail.com>
2024-06-24 09:10:58 -0300

    Adding vGPU nodes for GPU PCS session

--- main.tf
+++ main.tf
@@ -20,9 +20,9 @@ module "openstack" {
   instances = {
     mgmt    = { type = "p8-12gb", tags = ["puppet", "mgmt", "nfs"], disk_size=50, count = 1 }
     login   = { type = "p8-12gb", tags = ["login", "public", "proxy"], disk_size=50, count = 1 }
-    node16c = { type = "c16-60gb-392", tags = ["node"], count = 1 }
+    node16c = { type = "c16-60gb-392", tags = ["node"], count = 0 }
     #node4c1g = { image="Rocky-8.9-x64-2023-11",type = "g1-8gb-c4-22gb", tags = ["node"], count = 1 }
-    node4c1g = { image="Rocky-8.10-x64-2024-06",type = "g1-8gb-c4-22gb", tags = ["node"], count = 1 }
+    node4c1g = { image="Rocky-8.10-x64-2024-06",type = "g1-8gb-c4-22gb", tags = ["node"], count = 15 }
   }
 
   # var.pool is managed by Slurm through Terraform REST API.
